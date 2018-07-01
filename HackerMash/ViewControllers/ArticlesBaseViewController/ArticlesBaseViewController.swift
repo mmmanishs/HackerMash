36b71e44ebdf2e023e66e009b2e16f7b61fc15c5
Manish Singh <mmmanishs@gmail.com>
2018-07-01 17:17:22 -0400

    wip# Please enter the commit message for your changes. Lines starting

---
+++ HackerMash/ViewControllers/ArticlesBaseViewController/ArticlesBaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ArticlesBaseViewController.swift
+//  HackerMash
+//
+//  Created by Manish Singh on 7/1/18.
+//  Copyright © 2018 Manish Singh. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesBaseViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
